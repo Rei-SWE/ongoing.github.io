a06fb1e91e2c00552235a97f1bc35666e06b73ca
ONSAEMIRO <168421674+ongoig@users.noreply.github.com>
2024-07-07 09:49:06 +0900

    VA:CO 실시간 메신저 프로젝트

---
+++ src/js/config.js
@@ -0,0 +1,11 @@
+// [주의] 붙여넣기 하는 과정에서 export 키워드를 삭제하지 않도록 주의하세요!
+export const firebaseConfig = {
+  apiKey: "AIzaSyAR1fHW0heyc5vNgsgiRFhTJk9U_27BxI4",
+  authDomain: "vaco-messenger-eb2e3.firebaseapp.com",
+  projectId: "vaco-messenger-eb2e3",
+  storageBucket: "vaco-messenger-eb2e3.appspot.com",
+  messagingSenderId: "875222121687",
+  appId: "1:875222121687:web:d9fdd558ef9dc8a94a8492",
+  measurementId: "",
+  databaseURL: "https://vaco-messenger-eb2e3-default-rtdb.asia-southeast1.firebasedatabase.app/",
+};
